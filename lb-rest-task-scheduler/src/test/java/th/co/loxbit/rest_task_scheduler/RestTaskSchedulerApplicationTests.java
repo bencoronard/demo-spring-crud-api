22a0bd0dd329dc9a580c9a03c87234ca88b75ef5
Ben <128577990+bencoronard@users.noreply.github.com>
2024-08-29 13:53:52 +0700

    Initialize project

---
+++ lb-rest-task-scheduler/src/test/java/th/co/loxbit/rest_task_scheduler/RestTaskSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package th.co.loxbit.rest_task_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTaskSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
