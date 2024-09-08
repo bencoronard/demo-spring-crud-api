ef3bb4f7a958d7d09e2136e08ce7c28f9a94b0be
Ben <128577990+bencoronard@users.noreply.github.com>
2024-09-08 22:23:29 +0700

    Add retryable exceptions

--- lb-rest-task-scheduler/src/main/java/th/co/loxbit/rest_task_scheduler/common/factories/RetryTemplateFactory.java
+++ lb-rest-task-scheduler/src/main/java/th/co/loxbit/rest_task_scheduler/common/factories/RetryTemplateFactory.java
@@ -1,16 +1,24 @@
 package th.co.loxbit.rest_task_scheduler.common.factories;
 
+import java.util.List;
+
 import org.springframework.retry.support.RetryTemplate;
 import org.springframework.retry.support.RetryTemplateBuilder;
 import org.springframework.stereotype.Component;
 
+import th.co.loxbit.rest_task_scheduler.common.http.exceptions.Resp5xxException;
+
 @Component
 public class RetryTemplateFactory {
+
+  private static final List<Class<? extends Throwable>> RETRYABLE_EXCEPTIONS = List.of(
+      Resp5xxException.class);
 
   public RetryTemplate withFixedBackOff(int maxAttempts, long backoffMillis) {
     return new RetryTemplateBuilder()
         .maxAttempts(maxAttempts)
         .fixedBackoff(backoffMillis)
+        .retryOn(RETRYABLE_EXCEPTIONS)
         .build();
   }
 
@@ -18,6 +26,7 @@ public class RetryTemplateFactory {
     return new RetryTemplateBuilder()
         .maxAttempts(maxAttempts)
         .exponentialBackoff(initInterval, multiplier, maxInterval)
+        .retryOn(RETRYABLE_EXCEPTIONS)
         .build();
   }
 
