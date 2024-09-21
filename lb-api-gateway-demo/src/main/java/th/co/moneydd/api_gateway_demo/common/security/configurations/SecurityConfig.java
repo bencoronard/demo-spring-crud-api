2ce84af6471c3173557b0a02801cedb409b1adde
Benjamin <128577990+bencoronard@users.noreply.github.com>
2024-09-21 20:27:15 +0700

    Remove Spring Security

--- lb-api-gateway-demo/src/main/java/th/co/moneydd/api_gateway_demo/common/security/configurations/SecurityConfig.java
+++ lb-api-gateway-demo/src/main/java/th/co/moneydd/api_gateway_demo/common/security/configurations/SecurityConfig.java
@@ -3,10 +3,6 @@ package th.co.moneydd.api_gateway_demo.common.security.configurations;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 import th.co.moneydd.api_gateway_demo.common.security.filters.ApiKeyFilter;
 
 @Configuration
@@ -21,29 +17,6 @@ public class SecurityConfig {
 
   // ---------------------------------------------------------------------------//
   // Configurations
-  // ---------------------------------------------------------------------------//
-
-  @Bean
-  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-    http.cors(
-        cors -> cors.disable());
-
-    http.csrf(
-        csrf -> csrf.disable());
-
-    http.authorizeHttpRequests(
-        authorize -> authorize.anyRequest().permitAll());
-
-    http.headers(headers -> headers.frameOptions(frame -> frame.disable()));
-
-    http.addFilterBefore(apiKeyFilter(), UsernamePasswordAuthenticationFilter.class);
-
-    return http.build();
-  }
-
-  // ---------------------------------------------------------------------------//
-  // Methods
   // ---------------------------------------------------------------------------//
 
   @Bean
