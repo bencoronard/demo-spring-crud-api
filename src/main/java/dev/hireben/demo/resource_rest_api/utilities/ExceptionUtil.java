9e8fc8b99744e6950b32b48c641b84b764b77a3a
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-14 22:28:24 +0700

    Add utilities module

--- src/main/java/dev/hireben/demo/resource_rest_api/utilities/ExceptionUtil.java
+++ src/main/java/dev/hireben/demo/resource_rest_api/utilities/ExceptionUtil.java
@@ -1,28 +1,30 @@
 package dev.hireben.demo.resource_rest_api.utilities;
 
 import dev.hireben.demo.resource_rest_api.exceptions.SeverityLevel;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public class ExceptionUtil {
 
   // ---------------------------------------------------------------------------//
   // Fields
   // ---------------------------------------------------------------------------//
 
-  private static final String DEBUG_STRING_FORMAT = "%s[code=%s, severity=%s, message=%s]";
-  private static final String DEBUG_LOG_FORMAT = "Trace: %s >>> %s";
+  private final String DEBUG_STRING_FORMAT = "%s[code=%s, severity=%s, message=%s]";
+  private final String DEBUG_LOG_FORMAT = "Trace: %s >>> %s";
 
   // ---------------------------------------------------------------------------//
   // Methods
   // ---------------------------------------------------------------------------//
 
-  public static String formatDebugString(Class<? extends Throwable> clazz, String code, SeverityLevel severity,
+  public String formatDebugString(Class<? extends Throwable> clazz, String code, SeverityLevel severity,
       String message) {
     return String.format(DEBUG_STRING_FORMAT, clazz.getName(), code, severity.name(), message);
   }
 
   // ---------------------------------------------------------------------------//
 
-  public static String formatTraceLog(String traceId, String message) {
+  public String formatTraceLog(String traceId, String message) {
     return String.format(DEBUG_LOG_FORMAT, traceId, message);
   };
 
