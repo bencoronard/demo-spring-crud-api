74da6bb91af77c175511e65d10dddae05f65cc25
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-18 11:19:51 +0700

    Add UseCaseConfig

---
+++ src/main/java/dev/hireben/demo/rest/resource/infrastructure/configuration/UseCaseConfig.java
@@ -0,0 +1,45 @@
+package dev.hireben.demo.rest.resource.infrastructure.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import dev.hireben.demo.rest.resource.application.usecase.CreateResourceUseCase;
+import dev.hireben.demo.rest.resource.application.usecase.DeleteResourceUseCase;
+import dev.hireben.demo.rest.resource.application.usecase.RetrieveResourceUseCase;
+import dev.hireben.demo.rest.resource.application.usecase.UpdateResourceUseCase;
+import dev.hireben.demo.rest.resource.domain.repository.ResourceRepository;
+
+@Configuration
+public class UseCaseConfig {
+
+  // ---------------------------------------------------------------------------//
+  // Methods
+  // ---------------------------------------------------------------------------//
+
+  @Bean
+  CreateResourceUseCase createResourceUseCase(ResourceRepository resourceRepository) {
+    return new CreateResourceUseCase(resourceRepository);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  @Bean
+  RetrieveResourceUseCase retrieveResourceUseCase(ResourceRepository resourceRepository) {
+    return new RetrieveResourceUseCase(resourceRepository);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  @Bean
+  UpdateResourceUseCase updateResourceUseCase(ResourceRepository resourceRepository) {
+    return new UpdateResourceUseCase(resourceRepository);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  @Bean
+  DeleteResourceUseCase deleteResourceUseCase(ResourceRepository resourceRepository) {
+    return new DeleteResourceUseCase(resourceRepository);
+  }
+
+}
