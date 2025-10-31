affb0ec36b3db8282622c01b9c69044dfdaa497a
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-10-31 09:09:55 +0700

    Update access modifier of entry points

--- src/test/java/dev/hireben/demo/crud_api/CrudApiApplicationTests.java
+++ src/test/java/dev/hireben/demo/crud_api/CrudApiApplicationTests.java
@@ -2,9 +2,11 @@ package dev.hireben.demo.crud_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class CrudApiApplicationTests {
+@ActiveProfiles("test")
+final class CrudApiApplicationTests {
 
 	@Test
 	void contextLoads() {
