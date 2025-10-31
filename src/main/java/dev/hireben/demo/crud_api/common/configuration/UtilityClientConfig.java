06217eeebf91463f830074c0bd686d28f9e01af1
ben@orion <128577990+bencoronard@users.noreply.github.com>
2025-10-31 16:57:13 +0700

    Init utility client config

--- src/main/java/dev/hireben/demo/crud_api/common/configuration/UtilityClientConfig.java
+++ src/main/java/dev/hireben/demo/crud_api/common/configuration/UtilityClientConfig.java
@@ -1,14 +1,19 @@
 package dev.hireben.demo.crud_api.common.configuration;
 
+import java.security.KeyPair;
+import java.security.PublicKey;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 
+import dev.hireben.demo.common_libs.factory.JwtClientFactory;
 import dev.hireben.demo.common_libs.http.factory.RestClientFactory;
+import dev.hireben.demo.common_libs.utility.JwtClient;
 
 @Configuration
-class RestClientConfig {
+class UtilityClientConfig {
 
   @Bean
   RestClient restClient(
@@ -17,6 +22,18 @@ class RestClientConfig {
       @Value("${internal.http.client.timeout.conn-req-in-sec}") Integer connReqTimeout) {
 
     return RestClientFactory.buildRestClient(connTimeout, readTimeout, connReqTimeout);
+  }
+
+  // -----------------------------------------------------------------------------
+
+  @Bean
+  JwtClient jwtClientVerifier(
+      @Value("${internal.jwt.verify-key-path}") String keyPath) {
+
+    PublicKey publicKey = null;
+    KeyPair keyPair = new KeyPair(publicKey, null);
+
+    return JwtClientFactory.buildJwtClientWithAsymmmetricKeys(null, keyPair);
   }
 
 }
